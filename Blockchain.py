cc6731aca695786e21099abb62510e7b3c72fbf6
ghadakhedri1 <ghada.khedri1@esprit.tn>
2022-01-11 22:37:03 +0100

    Creating a Blockchain

--- Blockchain.py
+++ Blockchain.py
@@ -1,10 +1,4 @@
 # Module 1 - Create a Blockchain
-
-# To be installed:
-# Flask==0.12.2: pip install Flask==0.12.2
-# Postman HTTP Client: https://www.getpostman.com/
-
-# Importing the libraries
 import datetime
 import hashlib
 import json
